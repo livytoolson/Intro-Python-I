bc7afe76365d1c7b29f579487db1dbb306dd760c
Olivia Toolson <livytoolson@gmail.com>
2021-01-06 09:15:29 -0700

    Day 3

--- src/14_cal.py
+++ src/14_cal.py
@@ -27,6 +27,88 @@ print out a calendar for April in 2015, but if you omit either the year or both 
 it should use today’s date to get the month and year.
 """
 
+# datetime is useful to get the current time stamp
+# sys.argv: anything you pass in after the file name, when you run the file it can take it in as an argument (information that you want)
+# take in arguments from the command line using sys.argv
+# study documentation on calendar and datetime
+# command line arguments are returned as strings
+
 import sys
 import calendar
 from datetime import datetime
+
+date = datetime.now() # gets todays date
+current_month = date.month # accessing current month through the stored date variable
+current_year = date.year # accessing current year through the stored date variable 
+if len(sys.argv) == 1:
+  month = current_month # assigning month and year to current_month and current_year
+  year = current_year
+elif len(sys.argv) == 2:
+  month = int(sys.argv[1]) # position 0 is the file name & arguments start after that
+  year = current_year
+else:
+  month = int(sys.argv[1])
+  year = int(sys.argv[2])
+
+cal = calendar.TextCalendar().formatmonth(year, month) # "prmonth" shows "None" in terminal
+print(cal)
+
+# DOC'S CODE -->
+
+# import sys
+# import calendar
+# from datetime import datetime
+# # $%$Start
+# l = len(sys.argv)
+# if l == 1:
+#     # User didn't specify any input
+#     month = datetime.now().month
+#     year = datetime.now().year
+# elif l == 2:
+#     # User didn't specify year
+#     month = int(sys.argv[1])
+#     year = datetime.now().year
+# elif l == 3:
+#     # User specified both month and year
+#     month = int(sys.argv[1])
+#     year = int(sys.argv[2])
+# else:
+#     # User provided faulty input
+#     print("usage: calendar.py [month] [year]")
+#     sys.exit(1)
+# cal = calendar.TextCalendar()
+# cal.prmonth(year, month)
+
+
+# JOANNE'S CODE -->
+
+# import sys
+# import calendar as cal
+# from datetime import datetime as dt
+# # Create variable to hold today's date
+# today = dt.today()
+# # Get the user's input from the command line and check edge cases
+# # If user enters all required inputs in the command line
+# if len(sys.argv) == 3:
+#     # Convert user input to ints and return the calender month and year
+#     print(cal.TextCalendar().formatmonth(int(sys.argv[2]), int(sys.argv[1])))
+#     sys.exit(0)  # Exits the program
+# # If the user only enters one argument, such as month but not year or vise versa
+# elif len(sys.argv) == 2:
+#     # Return current in place of missing value from user input
+#     if int(sys.argv[1]) in range(12):
+#         print(cal.TextCalendar().formatmonth(today.year, int(sys.argv[1])))
+#         sys.exit(0)  # Exits the program
+#     elif int(sys.argv[1]) in range(1000, 2021):
+#         print('Year', int(sys.argv[1]))
+#         print(cal.TextCalendar().formatmonth(int(sys.argv[1]), today.month))
+#         sys.exit(0)  # Exits the program
+# # If the user does not enter and arguments in the command line
+# if len(sys.argv) == 1:
+#     # Return current month and year
+#     print(cal.TextCalendar().formatmonth(today.year, today.month))
+#     sys.exit(0)  # Exits the program
+# # If user enters an invalid format
+# else:
+#     print('Usage: 14_cal.py [month number] [year]')  # Return usage statement
+#     sys.exit(1)  # Exit the program
--- src/15_classes.py
+++ src/15_classes.py
@@ -1,21 +1,54 @@
+# Object oriented programming (inheritance, dependency injection, abstraction)
+# inheritance is being reinforced in this file - create a class and pass it's methods and variables to a new class
+# read about the dunder __repr__  and dunder __str__ methods (dunder methods, protocal, magic methods)
+# in terminal --> python3 --> dir(name of method) 
+
 # Make a class LatLon that can be passed parameters `lat` and `lon` to the
 # constructor
 
 # YOUR CODE HERE
+class LatLon: # base class
+    def __init__(self, lat, lon):           # self is a reference to the object the method is being invoked on (lat, lon object), binds the attributes
+        self.lat = lat
+        self.lon = lon
 
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
 # YOUR CODE HERE
+class Waypoint(LatLon):                     # Waypoint is a child of LatLon
+    def __init__(self, name, lat, lon):     # initializer
+        self.name = name
+        super().__init__(lat, lon)          # allows us to overwrite values of lat and lon
+    def __str__(self):                      # returns the string representation of the object -- makes the object human readable, using this method when calling print method under the hood
+        return f'name: {self.name}, lat: {self.lat}, lon: {self.lon}'
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
 # YOUR CODE HERE
+class Geocache(Waypoint):                   # Geocache is a grandchild of LatLon
+    def __init__(self, name, difficulty, size, lat, lon):
+        self.difficulty = difficulty
+        self.size = size
+        super().__init__(name, lat, lon)
+    def __str__(self):
+        return f"""         
+        {self.name},
+        diff {self.difficulty},
+        size {self.size},            
+        {self.lat},
+        {self.lon}
+        """
+# triple quotes = docstring -- prints values on different lines
+# useful to use docstrings for menu items
+# /n (new line character) also prints values on new line
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
 # YOUR CODE HERE
+# creating a new instance of the class -->
+waypoint = Waypoint("Catacombs", 41.70505, -121.51521)
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
@@ -24,6 +57,8 @@ print(waypoint)
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
 # YOUR CODE HERE
+# creating a new instance of the class -->
+geocache = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 
 # Print it--also make this print more nicely
 print(geocache)
